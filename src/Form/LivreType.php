e9d15f03799624548ec9134ced2f9630ac4e6637
AniTecK <aniteck82@hotmail.fr>
2020-02-19 11:42:26 +0100

    added placeholders to form

--- src/Form/LivreType.php
+++ src/Form/LivreType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Livre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType as TypeIntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,24 @@ class LivreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('couverture')
-            ->add('auteur')
-            ->add('prix')
-            ->add('genre')
-            ->add('resume')
+            ->add('titre', TypeTextType::class, [
+                'attr' => ['placeholder' => 'Titre du livre']
+            ])
+            ->add('couverture', TypeTextType::class, [
+                'attr' => ['placeholder' => "Entrez l'URL de la couverture du livre"]
+            ])
+            ->add('auteur', TypeTextType::class, [
+                'attr' => ['placeholder' => "Entrez l'Auteur du livre"]
+            ])
+            ->add('prix', TypeIntegerType::class, [
+                'attr' => ['placeholder' => 'Entrez le prix du livre']
+            ])
+            ->add('genre', TypeTextType::class, [
+                'attr' => ['placeholder' => 'Entrez le genre du livre']
+            ])
+            ->add('resume', TypeTextType::class, [
+                'attr' => ['placeholder' => 'Entrez un bref résumé du livre']
+            ])
         ;
     }
 
